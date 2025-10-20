30f6d874971999d5047caa1cdc52e69d52377229
hounisen <thj@hounisen.com>
2025-10-20 09:07:09 +0200

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+```
+Commit filen.
+
+### **Fil 6: `.gitignore`**
+```
+node_modules
+dist
+.env
+.DS_Store
+.vercel
